d2a8e3cb2233484a80d88dae4eb993f7a94cea45
Louis Maddox <louismmx@gmail.com>
2024-07-21 12:57:30 +0100

    feat: working multi-command CLI

--- src/pydantree/interface/cli.py
+++ src/pydantree/interface/cli.py
@@ -10,22 +10,27 @@ from .models import GrammarConfig, NodeTypesConfig
 
 __all__ = ["run"]
 
-
 def handle_validation_error(ve: ValidationError) -> None:
     error_msgs = "\n".join(str(e["ctx"]["error"]) for e in ve.errors())
     msg = "Invalid command:\n" + indent(error_msgs, prefix="- ")
     print(msg, end="\n\n", file=stderr)
 
-
 def run():
     try:
-        config = defopt.run(GrammarConfig, no_negated_flags=True)
+        config = defopt.run(
+            {"grammar": GrammarConfig, "nodes": NodeTypesConfig}, no_negated_flags=True
+        )
     except ValidationError as ve:
         handle_validation_error(ve)
         try:
-            defopt.run(view_grammar, argv=["-h"], no_negated_flags=True)
+            defopt.run({"grammar": GrammarConfig, "nodes": NodeTypesConfig}, argv=["-h"], no_negated_flags=True)
         except SystemExit as exc:
             exc.code = 1
             raise
     else:
-        view_grammar(config=config)
+        match config:
+            case GrammarConfig():
+                view_grammar(config=config)
+            case NodeTypesConfig():
+                view_node_types(config=config)
+
